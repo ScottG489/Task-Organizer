81e9fe3f5db65e510bd098326fba0cc64b353add
Scott Giminiani <ScottG489@Gmail.com>
2011-07-16 15:10:01 -0400

    added all preliminary options and refactored

--- task.py
+++ task.py
@@ -1,15 +1,16 @@
 import textwrap
 class Task():
 	def __init__(self, title='', notes = '', priority = int(), tags = []):
+#		self.id
 		self.title = title
 		self.notes = notes
-		self.priority = priority
-		self.tags = tags
+#		self.priority = priority
+#		self.tags = tags
 
 	def __str__(self):
 		return textwrap.dedent('''\
 			Title: %(title)s
 			Notes: %(notes)s''') % {
 				'title': self.title,
-			'notes': self.notes
+				'notes': self.notes
 		}
