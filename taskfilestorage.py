a2778949814b5e955c452cab09cbefd74268c130
Scott Giminiani <ScottG489@Gmail.com>
2011-07-19 13:36:04 -0400

    Modified TaskFileStorage so add() returns the id of the added task and find() to accept an id

--- taskfilestorage.py
+++ taskfilestorage.py
@@ -1,16 +1,17 @@
 import pickle
-import task
 import re
 import keygenerator
 
 class TaskFileStorage():
+	def __init__(self):
+		self.file_name = 'taskfile'
+
 #TODO:Add error handing for malformed file
 #	* Make private?
 #	* Make it return an empty list instead of raise?
 #   * Duplicate functionality of read() in TaskFileStorage?
 	def read(self):
-		file_name = 'taskfile'
-		task_file = open(file_name, 'r')
+		task_file = open(self.file_name, 'r')
 		try:
 			task_list = pickle.load(task_file)
 			task_file.close()
@@ -21,7 +22,7 @@ class TaskFileStorage():
 
 #TODO:Make private?
 	def write(self, task_list):
-		task_file = open(file_name, 'w')
+		task_file = open(self.file_name, 'w')
 		pickle.dump(task_list, task_file, 0)
 		task_file.close()
 
@@ -38,17 +39,28 @@ class TaskFileStorage():
 		task_list.append(task_item)
 		self.write(task_list)
 
-	def find(self, regex):
+		return task_item.id
+
+#TODO:Validate the input regular expression here or somewhere
+#	* Return an int instead of a list when id is specified?
+	def find(self, regex=None, id=None):
+		if regex and id:
+			raise TypeError('find() takes exactly 1 argument (2 given)')
 		match_list = []
 		try:
 			task_list = self.read()
 		except (EOFError, IOError):	#No tasks
 			return match_list
 		
-##TODO:Validate the input regular expression here or somewhere
-		for task_item in task_list:
-			if re.search(r'' + regex + '', task_item.title):
-				match_list.append(task_item)
+		if regex != None:
+			for task_item in task_list:
+				if re.search(r'' + regex + '', task_item.title):
+					match_list.append(task_item)
+		elif id != None:
+			for task_item in task_list:
+				if id == task_item.id:
+					match_list.append(task_item)
+
 
 		return match_list
 	def delx(self):
--- testtaskfilestorage.py
+++ testtaskfilestorage.py
@@ -1,16 +1,30 @@
 import unittest
-import task
 import taskfilestorage
-## Is ther a simple way to test non-returning functions?
+import task
+import pickle
+
+## Is there a simple way to test non-returning functions?
 class TestTaskFileStorage(unittest.TestCase):
 	def setUp(self):
-		self.my_task = task.Task()
-		self.file_storage = taskfilestorage.TaskFileStorage()
+		
+		pass
+	
+	def test_read(self):
+#		file_storage = taskfilestorage.TaskFileStorage()
+#		file_storage.file_name
+#		task_file = open(file_storage.file_name, 'r')
+#		task_file.close()
+#		task_list = pickle.load(task_file)
+#		self.assertEqual(task_list, file_storage.read())
 		pass
 
 	def test_add(self):
-		#self.file_storage().add(my_task)
-		pass
+		my_task = task.Task()
+		my_task.title = 'title'
+		my_task.notes = 'note'
+		file_storage = taskfilestorage.TaskFileStorage()
+		id = file_storage.add(my_task).id
+
 	
 	def test_find(self):
 		pass
