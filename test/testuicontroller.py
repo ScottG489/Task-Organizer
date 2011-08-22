a1f9d8f93c773f10872c9f42c76c2c9eff8cd6e0
Scott Giminiani <ScottG489@Gmail.com>
2011-08-22 15:33:35 -0400

    Added test_find_all() and test_edit_none() to TestUiController to increase code coverage.

--- test/testuicontroller.py
+++ test/testuicontroller.py
@@ -41,6 +41,21 @@ class TestUIController(unittest.TestCase):
                 [found_task.title, found_task.notes],
                 [self.title, self.notes])
 
+    def test_find_all(self):
+        added_task = self.add_task()
+        self.key = added_task.key
+
+        action_dict = {
+                'sub_cmd':  'find',
+                'key':      None}
+
+        task_list = self.ui.find(action_dict)
+
+        self.assertEqual(
+                [task_list[0].title, task_list[0].notes],
+                [added_task.title, added_task.notes])
+
+
     def test_edit(self):
         added_task = self.add_task()
         self.key = added_task.key
@@ -56,6 +71,23 @@ class TestUIController(unittest.TestCase):
 
         self.assertEqual(new_task.title, old_task.title)
         self.assertNotEqual(new_task.notes, old_task.notes)
+
+    def test_edit_none(self):
+        added_task = self.add_task()
+        self.key = added_task.key
+
+        action_dict = {
+                'sub_cmd': 'edit',
+                'title': None,
+                'notes': None,
+                'key': self.key}
+
+        old_task = self.ui.edit(action_dict)
+        new_task = self.find_task()
+
+        self.assertEqual(
+                [old_task.title, old_task.notes],
+                [new_task.title, new_task.notes])
 
     def test_delete(self):
         added_task = self.add_task()
