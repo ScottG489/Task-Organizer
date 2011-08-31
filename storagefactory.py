a98fe788ae587c2aeb7458d58227444211f46dfb
Scott Giminiani <ScottG489@Gmail.com>
2011-08-31 13:22:26 -0400

    Adds Docstrings to get() for StorageFactory and UIControllerFactory.

--- storagefactory.py
+++ storagefactory.py
@@ -10,6 +10,15 @@ class StorageFactory():
             self, 
             storage_type, 
             **kwargs):
+        """Return a Task storage instance.
+
+        Arguments:
+        storage_type -- name of the desired storage type
+        kwargs -- keyword arguments specific to each storage type
+
+        Using the given storage type, create an instance with the given
+        keyword arguments and return the storage instance.
+        """
         if storage_type == 'file':
             try:
                 del kwargs['task_dbname']
